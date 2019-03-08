5206d8b76c6a9c739243636b8b161f37f1cfb715
Sam Killermann <sK@hues.xyz>
2019-03-08 11:17:09 -0600

    new carousel with way less javascript

--- themes/facilitator-cards-theme/static/js/scripts.js
+++ themes/facilitator-cards-theme/static/js/scripts.js
@@ -1,17 +1,7 @@
-//@prepros-prepend plugins/flickity.pkgd.min.js
 //@prepros-prepend plugins/vanilla-tilt.min.js
 //@prepros-prepend plugins/wow.min.js
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'center',
-  wrapAround: true,
-  autoPlay: true,
-  pageDots: false
-});
-
-VanillaTilt.init(document.querySelectorAll(".aside .card"), {
+VanillaTilt.init(document.querySelectorAll(".sidebar .card"), {
 	glare: true,
     reset: false
 });
